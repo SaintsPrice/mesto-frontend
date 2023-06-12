590fca03824142c6073627ea19f1412f1f22e583
Hokage <famous.price@gmail.com>
2023-06-12 11:58:52 +0300

    edit https

--- src/http/index.js
+++ src/http/index.js
@@ -20,7 +20,7 @@ api.interceptors.response.use((config) => {
   if(error.response.status === 401 && error.config && !error.config._isRetry) {
     originalRequest._isRetry = true
     try {
-      const response = await axios.get(`${API_URL}/refresh`, {withCredentials: true})
+      const response = await axios.post(`${API_URL}/refresh`, {withCredentials: true})
 
       localStorage.setItem('token', response.data.acessToken)
       return api.request(originalRequest)
